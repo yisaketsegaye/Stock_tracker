a4103d7a080d57f3de65b3d1badf0f9922d0ed6a
yisaketsega <yisaketsega@gmail.com>
2018-09-01 16:48:04 -0400

    navigation and full_name and last_name added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   
-  def my_protfolio
-    
+def my_portfolio
+    @user = current_user
+    @user_stocks = current_user.stocks
   end
+ 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
- root 'welcome#index'
+ 
+  devise_for :users,:controllers => { :registrations => "user/registrations" } 
+  root 'welcome#index'
   get 'my_portfolio' , to: "users#my_portfolio"
   get 'search_stock' , to: "stocks#search"
+  resources :user_stocks, only: [:create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
